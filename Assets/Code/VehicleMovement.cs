7ca94a47fc4fee9411c603629c3e1f5f2ba2c8fc
Matthew Russell Downey <matthew.r.downey@gmail.com>
2020-01-31 22:08:22 -0500

    Vehicle code

---
+++ Assets/Code/VehicleMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleMovement : MonoBehaviour
+{
+    Transform vehicle_transform;
+
+    float vehicle_speed = 6f;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        vehicle_transform = this.GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        vehicle_transform.position += 
+    }
+}
